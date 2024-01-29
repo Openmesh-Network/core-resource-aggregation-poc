66c029c6efa38a5fef6a273f7cfa0a648e3fcaf5
classmateada <classmateada@gmail.com>
2024-01-29 18:45:50 +1100

    Add main function and top-level instance

--- cmd/poc.go
+++ cmd/poc.go
@@ -1,4 +1,54 @@
 package main
 
+import (
+    "context"
+    "log"
+    "openmesh.network/aggregationpoc/internal/instance"
+    "os"
+    "os/signal"
+    "strconv"
+    "strings"
+    "syscall"
+    "time"
+)
+
 func main() {
+    // Read instance name, gossip port, and known peers from the environment
+    // XNODE_NAME: string
+    peerName := os.Getenv("XNODE_NAME")
+    if peerName == "" {
+        peerName = "Xnode-1"
+    }
+    // XNODE_GOSSIP_PORT: number
+    gossipPort, _ := strconv.Atoi(os.Getenv("XNODE_GOSSIP_PORT"))
+    if gossipPort == 0 {
+        gossipPort = 9090
+    }
+    // XNODE_KNOWN_PEERS: addresses split by comma (,)
+    // e.g., 127.0.0.1:9090,127.0.0.1:9091
+    knownPeersString := os.Getenv("XNODE_KNOWN_PEERS")
+    var knownPeers []string
+    if knownPeersString != "" {
+        knownPeers = strings.Split(knownPeersString, ",")
+    }
+
+    // Initialise graceful shutdown
+    cancelCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    sigChan := make(chan os.Signal, 1)
+    signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+    // Initialise and start the instance
+    pocInstance := instance.NewInstance(peerName, gossipPort)
+    pocInstance.Start(cancelCtx, knownPeers)
+
+    // Stop here!
+    sig := <-sigChan
+    log.Printf("Termination signal received: %v", sig)
+
+    // Cleanup
+    if err := pocInstance.Gossip.Leave(); err != nil {
+        log.Printf("Failed to leave the cluster: %s", err.Error())
+    }
 }
--- internal/instance/instance.go
+++ internal/instance/instance.go
@@ -1,1 +1,24 @@
 package instance
+
+import (
+    "context"
+    "openmesh.network/aggregationpoc/internal/gossip"
+)
+
+// Instance is the top-level instance of the whole poc project
+type Instance struct {
+    Gossip *gossip.Instance // Member management and data sharing
+}
+
+// NewInstance create all the low-level instances, then create the top-level instance
+func NewInstance(instanceName string, gossipPort int) *Instance {
+    gi := gossip.NewInstance(instanceName, gossipPort)
+    return &Instance{
+        Gossip: gi,
+    }
+}
+
+// Start starts all the instances, then start the top-level instance
+func (i *Instance) Start(ctx context.Context, knownPeers []string) {
+    i.Gossip.Start(ctx, knownPeers)
+}
