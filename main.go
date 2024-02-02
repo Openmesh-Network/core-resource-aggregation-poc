42b0cf903e894f4a974e0ab28f6523eb2fb2e9e6
dowdt <tomas.dowd@openmesh.network>
2024-02-02 11:51:28 +1100

    now the ipfs nodes connect on libp2p

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"context"
 	"log"
-	"openmesh.network/aggregationpoc/internal/instance"
 	"os"
 	"os/signal"
 	"strconv"
 	"strings"
 	"syscall"
+
+	"openmesh.network/aggregationpoc/internal/instance"
 )
 
 func main() {
 	// Read instance name, gossip port, and known peers from the environment
 	// XNODE_NAME: string
+
+	log.Println("Calling gossip peers")
+
 	peerName := os.Getenv("XNODE_NAME")
 	if peerName == "" {
 		peerName = "Xnode-1"
@@ -35,10 +39,13 @@ func main() {
 	}
 
 	{
+		name := os.Getenv("XNODE_NAME")
+		log.Println("My name is:", name)
 		gossipPeersString := os.Getenv("XNODE_GOSSIP_PEERS")
 		if gossipPeersString != "" {
 			gossipPeers = strings.Split(gossipPeersString, ",")
 		}
+		log.Println("Got", len(gossipPeers), "peers", gossipPeers, "from:", gossipPeersString)
 
 		httpPeers = make([]string, len(gossipPeers))
 
@@ -51,9 +58,10 @@ func main() {
 	// Initialise graceful shutdown
 	cancelCtx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	defer log.Println("Calling cancel")
 
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
 
 	// Initialise and start the instance
 	pocInstance := instance.NewInstance(peerName, gossipPort, httpPort)
